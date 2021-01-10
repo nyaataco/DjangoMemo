54e419290ba9166c085ac38fa360172238df7ae4
nyaataco <sarah.samsara@gmail.com>
2021-01-10 17:00:07 +0900

    first commit

---
+++ memo/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Memo(models.Model):
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ memo/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Memo
+from .forms import MemoForm
+
+
+def show(request):
+    memo_id = request.GET.get('id')
+    memo = Memo.objects.get(pk=memo_id)
+    return HttpResponse('%s' % (memo.content))
+
+
+def create(request):
+    if request.method == 'GET':
+        form = MemoForm()
+        return render(request, 'create.html', {'form': form})
+
+    elif request.method == 'POST':
+        form = MemoForm(request.POST)
+        if form.is_valid():
+            memo = form.save(commit=False)
+            memo.save()
+            return redirect('/memo?id=%s' % memo.pk)
+
